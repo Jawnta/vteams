8e71a6e4b451809e893f3d494fa6e411058b8d6c
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-11-30 11:10:09 +0100

    fixed eslint errors for routes

--- packages/api/routes.ts
+++ packages/api/routes.ts
@@ -1,13 +1,22 @@
+import * as chargingStations from './routes/chargingstations.route';
+import * as chargingZones from './routes/chargingzones.route';
+import * as cities from './routes/cities.route';
+import * as invoices from './routes/invoices.route';
+import * as parkingZones from './routes/parkingzones.route';
+import * as scooters from './routes/scooters.route';
+import * as trips from './routes/trips.route';
+import * as users from './routes/users.route';
+
 module.exports = function (app: {use: (arg0: string, arg1: any) => void}) {
     /*
      * Routes
      */
-    app.use('/chargingstations', require('./routes/chargingstations.route'));
-    app.use('/chargingzones', require('./routes/chargingzones.route'));
-    app.use('/cities', require('./routes/cities.route'));
-    app.use('/invoices', require('./routes/invoices.route'));
-    app.use('/parkingzones', require('./routes/parkingzones.route'));
-    app.use('/scooters', require('./routes/scooters.route'));
-    app.use('/trips', require('./routes/trips.route'));
-    app.use('/users', require('./routes/users.route'));
+    app.use('/chargingstations', chargingStations);
+    app.use('/chargingzones', chargingZones);
+    app.use('/cities', cities);
+    app.use('/invoices', invoices);
+    app.use('/parkingzones', parkingZones);
+    app.use('/scooters', scooters);
+    app.use('/trips', trips);
+    app.use('/users', users);
 };
