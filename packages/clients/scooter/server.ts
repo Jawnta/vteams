d2b4ae225df5520d29965aeb1a9f8d70b279ec44
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-12-08 17:26:56 +0100

    scooter api

--- packages/clients/scooter/server.ts
+++ packages/clients/scooter/server.ts
@@ -1,8 +1,30 @@
-// create / simulate
-// GET scooters from database
-// for scooter in db:
-//      scoot = new Scooter(scooter.id, scooter.position);
-//      scoot.sleep();
+import express from 'express';
+const PORT = process.env.PORT || 1337;
+const app = express();
+app.set('port', PORT);
 
-// deallocate/:id
-//
+import Pool from "./pool";
+const pool = new Pool();
+pool.getScootersFromDb().then(r => 'what');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const result = pool.getScooters();
+        res.status(200).json(result);
+    } catch (err) {
+        return res.status(500).send({
+            error: err || 'Something went wrong.',
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(
+        `Express Server started on Port ${app.get(
+            'port'
+        )} | Environment : ${app.get('env')}`
+    );
+});
+
