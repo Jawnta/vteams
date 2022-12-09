069a796a1cf7f880c20f38d2b6c0cb56a34287ed
Malin Olsson <olssonmalin@live.com>
2022-12-09 12:21:01 +0100

    Add dummy data for parking, charging zone, charging station

---
+++ packages/database/sql/data/insert.sql
@@ -0,0 +1,15 @@
+use vteams;
+
+DELETE FROM charging_station;
+DELETE FROM charging_zone;
+DELETE FROM parking_zone;
+DELETE FROM scooter;
+DELETE FROM user;
+DELETE FROM city;
+
+SOURCE insert_city.sql;
+SOURCE insert_users.sql;
+SOURCE insert_scooters.sql;
+SOURCE insert_parking.sql;
+SOURCE insert_charging_zone.sql;
+SOURCE insert_charging_station.sql;
---
+++ packages/database/sql/data/insert_charging_station.sql
@@ -0,0 +1,13 @@
+USE 'vteams'
+INSERT INTO charging_station
+	(charging_zone_id, position)
+VALUES
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.15651577817534, 15.589940177964351]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.15638807550132, 15.58472108883886]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.15729575541694, 15.584258728798703]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.16039018160184, 15.590493886595402]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.1601674640998, 15.57903845871073]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.15812973559881, 15.5825217201215]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.15874511849343, 15.585522137443045]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.15777515962853, 15.585220367397321]}')),
+ ('1', ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [56.156048897692976, 15.583205037060106]}'));
