997b354c30de7f29ad327225ff7da3bd25f1c71e
Malin Olsson <olssonmalin@live.com>
2022-12-09 16:35:07 +0100

    Add dummy data for trip and invoice

--- packages/database/sql/data/insert.sql
+++ packages/database/sql/data/insert.sql
@@ -3,9 +3,11 @@ use vteams;
 DELETE FROM charging_station;
 DELETE FROM charging_zone;
 DELETE FROM parking_zone;
+DELETE FROM trip;
 DELETE FROM scooter;
 DELETE FROM user;
 DELETE FROM city;
+
 
 SOURCE insert_city.sql;
 SOURCE insert_users.sql;
@@ -13,3 +15,7 @@ SOURCE insert_scooters.sql;
 SOURCE insert_parking.sql;
 SOURCE insert_charging_zone.sql;
 SOURCE insert_charging_station.sql;
+SOURCE insert_trips.sql;
+SOURCE update_trips.sql;
+
+
